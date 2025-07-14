678d7b43eeb619b0ace19f33d5f9a577dcc8659c
sharath A L <alsharath66@gmail.com>
2025-07-14 23:06:47 +0530

    Second msg

--- Strings.py
+++ Strings.py
@@ -1,12 +1,21 @@
-# declare
-name="Sharath"
-print(name[-1])  #it will print the last letter so that python have negative indexing also
+# # declare
+# name="Sharath"
+# print(name[-1])  #it will print the last letter so that python have negative indexing also
 
-# String is imutable -- it cannot be changed once assigned
+# # String is imutable -- it cannot be changed once assigned
 
-#String slicing or indexing 
-name1="Sharah"
-print(name1[0:3])  #starts printing 0 to all the way till 3 (Exclusing 3)
+# #String slicing or indexing 
+# name1="Sharath"
+# print(name1[3:7])  #starts printing 3 to all the way till 7 (Exclusing 3)
+# print(name1[:4])  #same as 0 : 4
+# print(name1[0:])  #same as 0 : 4 
+
+#String Functions
+name="sharath"
+print(len(name)) #this function helps to find the length of the String
+print(name.endswith('a'))  #it will cheeck whether String ends with given char if yes it will print (True) else (False)
+print(name.startswith('s')) #it is opposite to endswith function both are case sensitive
+print(name.capitalize())  # it will convert first letter into capitial(only first alphabet)
 
 
 
